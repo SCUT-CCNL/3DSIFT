645ecec78d42678f9c54c7ee3e16c6441f475582
unknown <metalyounglot@gmail.com>
2020-05-06 18:17:16 +0800

    small modifications

--- 3DSIFT_mt/Example.cpp
+++ 3DSIFT_mt/Example.cpp
@@ -8,6 +8,7 @@ int main(){
 	const char *Ref = R"(D:\SIFT_EXPERI_DATA\Hole_intersect\624_500-Ori440Margin92_92_30-m12_std8_r8-intersect_6000-GN.bin)";
 	//const char *path_ = R"(F:\SIFT_EXPERI_LOG\DEBUG\Voxel_384_ori_GN_.txt)";
 
+	
 	int m, n, p;
 	float* SIFT_Vol = nullptr;
 	ReadMatrixFromDisk(Ref, &m, &n, &p, &SIFT_Vol);
